de013bdfaebaf49ad547cbe43a459b8c668d97e8
= <josephmckinsey2@gmail.com>
2017-11-02 16:58:10 -0600

    Add use of names and remove initial participation limit
    The previous version required the user to preselect the number of
    participants. This became annoying when someone new enters the room.
    Now you specify the stopping point for the algoirthm at each stage.
    
     * Query at beginning of use for names. Optional use.
     * Keep the structure of the program as close as possible to the original, so
       the program uses a few if statements with useNames as an operation.
     * Use a dictionary to define a number to a name.
        * This also applies to using numbers, for less bias, since the number of
          people in the index increased.
     * Create better checking for y/n along with default blank option capitalized
     * Lists winners at end of game.

--- busraffle.py
+++ busraffle.py
@@ -4,31 +4,84 @@ rng = SystemRandom()
 
 item = input("What are you giving away? ")
 n = int(input("How many {} are you giving away? ".format(item)))
-participants = int(input("How many people are participating? "))
-
-print("To start, participants 0 through {} get one of the {} (for now...)".format(n - 1, item))
-bus = list(range(n))
+temp = input("Do you want to enter names? (y/N) ")
 print()
+useNames = (temp == "yes" or temp == "y" or temp == "Yes" or temp == "Y")
+
+names = {}
+
+if useNames:
+    for i in range(n):
+        names[i] = input("Participant: ")
+        print("You get one of the {}! For now.".format(item))
+    print()
+else:
+    print("To start, participants 0 through {} get one of the {} (for now...)".format(n - 1, item))
+    for i in range(n):
+        names[i] = str(i)
+    print()
+
+bus =  list(range(n))
+
 print("List anyone who was gone, end with a blank line...")
 s = n
 while True:
     line = input()
     if line == '':
         break
-    print("Participant {} gets one of the {} (for now) then.".format(s, item))
-    bus[bus.index(int(line))] = s
-    s += 1
-
-j = n
-for e in range(s, participants):
-    here = input("Is participant {} here (y/n)? ".format(e)) == 'y'
-    if not here:
+    index = -1
+    for i, name in names.items():
+        if line == name:
+            index = i
+            break
+    if index == -1:
         continue
-    j = j + 1
-    if rng.random() < n / j:
+
+    if useNames:
+        names[index] = input("New Participant: ")
+    else:
+        names[index] = str(s)
+    s += 1
+    print("New Participant {} gets one of the {} (for now) then.".format(names[index], item))
+
+e = s
+j = n
+while True:
+    if useNames:
+        temp = input("Continue with participant (Y/n)? ")
+        quit = temp == 'y' or temp == 'Y' or temp == "yes" or temp == "Yes" or temp == ''
+        if not quit:
+            break
+    else:
+        temp = input("Continue with participant {} (Y/n)? ".format(e))
+        here = temp == 'y' or temp == 'Y' or temp == "yes" or temp == "Yes" or temp == ''
+        if not here:
+            temp = input("Quit entirely (y/N)? ")
+            if temp == 'y' or temp == 'Y' or temp == "yes" or temp == "Yes":
+                break
+            e += 1
+            continue
+
+    e = e + 1
+    if useNames:
+        temp = input("Participant: ")
+        names[j] = temp
+    else:
+        names[j] = e
+
+    if rng.random() < (n / len(names)):
         i = rng.randrange(0, n)
-        print("Participant {} steals one of the {} from participant {}!".format(e, item, bus[i]))
-        bus[i] = e
+        print("Participant {} steals one of the {} from participant {}!".format(names[j], item, names[bus[i]]))
+        bus[i] = j
     else:
         print("Nothing happened.")
+    j = j+1
 
+print()
+print("Winners are: ", end = '')
+for i in range(n - 1):
+    print(names[bus[i]], end = ', ')
+if n > 1:
+    print('and {}!'.format(names[bus[n-1]]))
+else:
+    print("{}!".format(names[bus[n-1]]))
