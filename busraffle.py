5d6e834a599b6a17110664f929a8811eefcdc7a5
= <josephmckinsey2@gmail.com>
2017-11-04 22:38:05 -0600

    Clean code and sane structuring
    Now uses code commenting, doc strings, and functions. The previous
    version used some while loops and unimportant temporary variables.
    
        * Add function to determine whether to stop.
        * Add function with y/n input.
        * Change while loops use condition instead of breakpoint.
        * Add doc strings to helper functions.
        * Clean code for printing winners.
        * Use more meaningful counter variables instead of e and s.
        * Add commenting.
        * Use __name__ == "__main__" and a main function.

--- busraffle.py
+++ busraffle.py
@@ -1,87 +1,151 @@
 from secrets import SystemRandom
 
-rng = SystemRandom()
 
-item = input("What are you giving away? ")
-n = int(input("How many {} are you giving away? ".format(item)))
-temp = input("Do you want to enter names? (y/N) ")
-print()
-useNames = (temp == "yes" or temp == "y" or temp == "Yes" or temp == "Y")
-
-names = {}
-
-if useNames:
-    for i in range(n):
-        names[i] = input("Participant: ")
-        print("You get one of the {}! For now.".format(item))
-    print()
-else:
-    print("To start, participants 0 through {} get one of the {} (for now...)".format(n - 1, item))
-    for i in range(n):
-        names[i] = str(i)
+def main():
+    # Initialize random number generator.
+    rng = SystemRandom()
+    
+    # Query user on raffle information
+    item = input("What are you giving away? ")
+    n = int(input("How many {} are you giving away? ".format(item)))
+    useNames = yes_no_check(input("Do you want to enter names? (y/N) "))
     print()
 
-bus =  list(range(n))
+    # Initialize names dictionary. Indexed at population number.
+    # Population number is independent from participant number.
+    names = {}
 
-print("List anyone who was gone, end with a blank line...")
-s = n
-while True:
+    # Using names requires entering names of each participant.
+    if useNames:
+        for i in range(n):
+            names[i] = input("Participant: ")
+            print("You get one of the {}! For now.".format(item))
+        print()
+    else:
+        print(
+            "To start, participants 0 through {} get one of the {} (for now...)".format(
+                n - 1, item))
+        # Sets name of each person into dictionary.
+        for i in range(n):
+            names[i] = str(i)
+        print()
+
+    # Define bus with first n people.
+    bus =  list(range(n))
+
+    print("List anyone who was gone, end with a blank line...")
+
+    # Participant number defined independently of population number.
+    participant_number = n
     line = input()
-    if line == '':
-        break
-    index = -1
-    for i, name in names.items():
-        if line == name:
-            index = i
-            break
-    if index == -1:
-        continue
-
-    if useNames:
-        names[index] = input("New Participant: ")
-    else:
-        names[index] = str(s)
-    s += 1
-    print("New Participant {} gets one of the {} (for now) then.".format(names[index], item))
-
-e = s
-j = n
-while True:
-    if useNames:
-        temp = input("Continue with participant (Y/n)? ")
-        quit = temp == 'y' or temp == 'Y' or temp == "yes" or temp == "Yes" or temp == ''
-        if not quit:
-            break
-    else:
-        temp = input("Continue with participant {} (Y/n)? ".format(e))
-        here = temp == 'y' or temp == 'Y' or temp == "yes" or temp == "Yes" or temp == ''
-        if not here:
-            temp = input("Quit entirely (y/N)? ")
-            if temp == 'y' or temp == 'Y' or temp == "yes" or temp == "Yes":
+    while line != "":
+        # Searches dictionary for name. If it appears,
+        # it breaks out of for loop and uses that index.
+        # -1 indicates not found
+        index = -1
+        for i, name in names.items():
+            if line == name:
+                index = i
                 break
-            e += 1
+
+        if index == -1:
             continue
 
-    e = e + 1
+        # Resets name entered. User may enter name or use next number 
+        if useNames:
+            names[index] = input("New Participant: ")
+        else:
+            names[index] = str(participant_number)
+        participant_number += 1
+        print(
+            "New Participant {} gets one of the {} (for now) then.".format(
+                names[index], item))
+        line = input()
+
+    # Name index is the population number entered into dictionary.
+    name_index = n
+    # User given option to quit or skip a number if not using names; updates part. number.
+    participant_number, more_participants = check_quitting(participant_number, useNames)
+    while more_participants:
+        if useNames:
+            name = input("Participant: ")
+            names[name_index] = name
+        else:
+            names[name_index] = participant_number
+
+        # Algorithm R sampling. See wikipedia. User steals from previous participants.
+        if rng.random() < (n / len(names)):
+            i = rng.randrange(0, n)
+            print(
+                "Participant {} steals one of the {} from participant {}!".format(
+                    names[name_index], item, names[bus[i]]))
+            bus[i] = name_index
+        else:
+            print("Nothing happened.")
+
+        name_index += 1
+        participant_number += 1
+        participant_number, more_participants = check_quitting(participant_number, useNames)
+
+    print()
+    print_winners(names, bus)
+
+
+def yes_no_check(input_string, default=False):
+    """Return boolean value of use input
+
+    Takes string and optional default value that returns with a blank value.
+    """
+    input_string = input_string.lower()
+    if (input_string == "y" or
+        input_string == "yes"):
+        return True
+    elif (input_string == 'n' or
+          input_string == 'no'):
+        return False
+
+    return default
+
+
+def check_quitting(participant_number, useNames):
+    """Query user whether to continue adding participants.
+
+    User may choose to skip numbers when not using names.
+    Otherwise, it will simply stop immediately.
+    """
     if useNames:
-        temp = input("Participant: ")
-        names[j] = temp
+        quit = yes_no_check(
+            input("Continue with participant {} (Y/n)? ".format(
+                participant_number)), True)
+        return participant_number, quit
     else:
-        names[j] = e
+        here = yes_no_check(
+            input("Continue with participant {} (Y/n)? ".format(
+                participant_number)), True)
+        if not here:
+            if yes_no_check(input("Quit entirely (y/N)? ")):
+                return participant_number, False
+            else:
+                # Skip number
+                participant_number += 1
+                return check_quitting(participant_number, useNames)
+        else:
+            return participant_number, True
 
-    if rng.random() < (n / len(names)):
-        i = rng.randrange(0, n)
-        print("Participant {} steals one of the {} from participant {}!".format(names[j], item, names[bus[i]]))
-        bus[i] = j
-    else:
-        print("Nothing happened.")
-    j = j+1
 
-print()
-print("Winners are: ", end = '')
-for i in range(n - 1):
-    print(names[bus[i]], end = ', ')
-if n > 1:
-    print('and {}!'.format(names[bus[n-1]]))
-else:
-    print("{}!".format(names[bus[n-1]]))
+def print_winners(names, bus):
+    """Prints winners after end of the raffle"""
+    if len(bus) == 1:
+        print("Winner is {}!".format(names[bus[0]]))
+    elif len(bus) == 2:
+        print("Winners are: {} and {}!".format(
+            names[bus[0]], names[bus[1]]))
+    elif len(bus) > 2:
+        print("Winners are: ", end='')
+        for i in bus[0:-1]:
+            print("{}, ".format(names[i]), end = '')
+        print("and {}!".format(names[bus[-1]]))
+
+
+if __name__ == "__main__":
+    main()
